3f9d2f068e8fa29600b59df4979abc4c62a00259
abys7315 <153502599+abys7315@users.noreply.github.com>
2025-08-30 02:07:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || process.env.VITE_CLIENT_URL,
+    origin: process.env.REACT_APP_CLIENT_URL || 'http://localhost:3000',
     credentials: true,
   })
 );
